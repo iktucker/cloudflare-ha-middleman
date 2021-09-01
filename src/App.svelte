c9693f70488e63d777ca743fe71609f8c3062333
Ian Tucker <issacnewtongravity@gmail.com>
2021-09-01 09:55:45 -0700

    Added svelte files

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<main>
+	<h1>Cloudflare HomeAssistant Middleware</h1>
+	<p>This is the Cloudflare Pages page for the HomeAssistant middleware.</p>
+	<p>
+		Hopefully this will describe how the middleware works at some point in the future. Right now it's just a demo so I can figure out how/why I'd want to use the Svelte framework on Pages.
+	</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
