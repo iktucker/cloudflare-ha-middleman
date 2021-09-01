cd097733d55184eb8eb7d90475a3e776c60068ec
Ian Tucker <issacnewtongravity@gmail.com>
2021-09-01 14:02:51 -0700

    Updated page content

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,18 @@
 <main>
-	<h1>Cloudflare HomeAssistant Middleware</h1>
+	<h1>Cloudflare Home Assistant Middleware</h1>
 	<p>This is the Cloudflare Pages page for the HomeAssistant middleware.</p>
 	<p>
 		Hopefully this will describe how the middleware works at some point in the future. Right now it's just a demo so I can figure out how/why I'd want to use the Svelte framework on Pages.
 	</p>
+	<h3>Home Assistant Configuration</h3>
+	<p>
+		Configure a rest sensor set to hit your cloudflare address as often as you'd like. I'd probably do every minute, but if you're trying to limit requests and traffic then you can make this longer.
+	</p>
+	<div style="padding: 3rem;">
+		<p>
+			Blerg
+		</p>
+	</div>
 </main>
 
 <style>
